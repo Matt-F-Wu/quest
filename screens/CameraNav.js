e34ef18ce68d969ef88e2b0dd6abbaa5e6df5709
Matt-F-Wu <haohao.wu@mail.utoronto.ca>
2018-01-23 23:58:50 -0800

    Real AR init, EXPO-THREE

--- screens/CameraNav.js
+++ screens/CameraNav.js
@@ -1,148 +1,84 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  Alert,
-  Dimensions,
-} from 'react-native';
+import Expo, { Asset } from 'expo';
+import React from 'react';
 
-import Colors from '../constants/Colors';
-import CameraBase from '../components/CameraBase';
-import Icon from 'react-native-vector-icons/Ionicons';
-var self;
-export default class CameraNav extends CameraBase {
-  static navigationOptions = ({ navigation, screenProps }) => ({
-    title:  'Navigate',
-    headerTintColor: Colors.tintColor,
-    headerLeft: (
-      <Icon name={'md-close-circle'} size={32} style={{padding: 10, marginLeft: 10, color: Colors.tintColor,}}
-                            onPress={ () => self.backToMain() } />
-      ),
-  });
+const THREE = require('three');
+global.THREE = THREE;
+import ExpoTHREE from 'expo-three'; // 2.0.2
 
-  constructor(props) {
-      super(props);
-      self = this;
+console.disableYellowBox = true;
+
+export default class App extends React.Component {
+  state = {
+    loaded: false,
   }
 
-  backToMain(){
-    const { navigate } = self.props.navigation;
-    self.setState({mountCam: false});
-    navigate('Profile');
+  componentWillMount() {
+    this.preloadAssetsAsync();
   }
 
-  num_objs = 5;
+  async preloadAssetsAsync() {
+    await Promise.all([
+      require('../assets/textures/coin/front.png'),
+      require('../assets/textures/coin/side.png'),
+      require('../assets/textures/coin/bottom.png')
+    ].map((module) => Expo.Asset.fromModule(module).downloadAsync()));
+    this.setState({ loaded: true });
+  }
 
-  customize(num) {
-    var i;
-    var objectArray = [];
-    var deviceWidth = Dimensions.get("window").width;
-    for(i = 0; i < num; i++){
-      objectArray.push({
-        x: (80-i*10) + '%',
-        y: (60-i*5) + '%',
-        w: (20-2*i)/100*deviceWidth,
-        h: (20-2*i)/100*deviceWidth,
-        image: require('../assets/images/coin.gif'),
-      });
-    }
+  render() {
+    return this.state.loaded ? (
+      <Expo.GLView
+        ref={(ref) => this._glView = ref}
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
+      />
+    ) : <Expo.AppLoading />;
+  }
 
-    objectArray.push({
-      x: (50 - (25 - i*5)/2) + '%',
-      y: (50 - (25 - i*5)/2) + '%',
-      w: (25 - i*5)/100*deviceWidth,
-      h: (25 - i*5)/100*deviceWidth,
-      image: require('../assets/images/star.gif'),
+  _onGLContextCreate = async (gl) => {
+    console.debug("Got here");
+
+    const width = gl.drawingBufferWidth;
+    const height = gl.drawingBufferHeight;
+    gl.createRenderbuffer = () => {};
+    gl.bindRenderbuffer = () => {};
+    gl.renderbufferStorage  = () => {};
+    gl.framebufferRenderbuffer  = () => {};
+
+    //ar init
+    const arSession = await this._glView.startARSessionAsync();
+
+    //three init
+    const scene = new THREE.Scene();
+    const camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(width, height);
+
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+    
+    const coinTexture = await ExpoTHREE.createTextureAsync({
+      asset: Asset.fromModule(require('../assets/images/coin.png')),
     });
-    //This view does not have coins/objects
-    this.setState({has_button: false, has_objects: true, 
-      objects: objectArray, styles: styles, has_lable: true,
-      lx: 10, ly: 10, 
-      textLable: 'Coins count: ' + (100 + (5-num)) + ', 150 meters utill destination'});
-  }
 
-  arrived() {
-    Alert.alert("You have arrived at your destination! \nPoint your camera to the sky!");
-    var i;
-    var objectArray = [];
-    var num_deceiver = 9;
-    var deviceWidth = Dimensions.get("window").width;
-    for(i = 0; i < num_deceiver; i++){
-      objectArray.push({
-        x: (Math.random()*88) + '%',
-        y: (Math.random()*88) + '%',
-        w: 12/100*deviceWidth,
-        h: 12/100*deviceWidth,
-        image: require('../assets/images/deceiver.png'),
-      });
+    const material = new THREE.MeshBasicMaterial( { map: coinTexture } );
+    //TODO: take 1/10 of the screen roughly, tune later
+    const geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.015, 100);
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.z = -0.4;
+    scene.add(cube);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      //x direction is pointing right, y axis is pointing upword
+      //cube.rotation.x += 0.01;
+      cube.rotation.x = 0.5;
+      cube.rotation.y += 0.02;
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
     }
-    const { navigate } = this.props.navigation;
-    objectArray.push({
-      x: (Math.random()*88) + '%',
-      y: (Math.random()*88) + '%',
-      w: 12/100*deviceWidth,
-      h: 12/100*deviceWidth,
-      image: require('../assets/images/goal.png'),
-      onClick: () => {this.num_objs = 5; this.setState({mountCam: false}); navigate('ViewQuest');},
-    })
-    //This view does not have coins/objects
-    this.setState({has_button: false, has_objects: true, 
-      objects: objectArray, styles: styles, has_lable: true,
-      textLable: 'Coins count: 105, at destination\nFind the odd ones out!'});
+    animate();   
   }
-
 }
 
-const styles = StyleSheet.create({
-  rowStyle: {
-    width: '100%',
-    height: '100%',
-  },
-  cardStyle: {
-    height: 0,
-  },
-  title: {
-    height: 0,
-  },
-  subTitle: {
-    height: 0,
-  },
-  text: {
-    height: 0,
-  },
-  lable: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 24,
-    padding: 5,
-    borderRadius: 4,
-    backgroundColor: Colors.blurOrange,
-  },
-  button: {
-    borderWidth: 1,
-    borderColor: Colors.blurText,
-    borderStyle: 'dotted',
-    borderRadius: 30,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    backgroundColor: Colors.tintColor,
-  },
-  buttonIconInner: {
-    color: 'white',
-    fontSize: 32,
-    width: 25,
-    height: 35
-  },
-  imageContainer: {
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  contactImg: {
-    width: '90%',
-    height: '90%',
-  },
-});
