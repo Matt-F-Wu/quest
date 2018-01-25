8951f5898ad33be85a9d23d044bbbb3d34429b69
Matt-F-Wu <haohao.wu@mail.utoronto.ca>
2018-01-25 01:43:18 -0800

    Google maps Direction API incorporated, coin now floats about actual direction of the route

--- screens/CameraNav.js
+++ screens/CameraNav.js
@@ -1,4 +1,4 @@
-import Expo, { Asset } from 'expo';
+import Expo, { Asset, Location, Permissions } from 'expo';
 import React from 'react';
 
 const THREE = require('three');
@@ -6,14 +6,19 @@ global.THREE = THREE;
 import ExpoTHREE from 'expo-three'; // 2.0.2
 
 console.disableYellowBox = true;
-
+var routeDecoder = require('./routeDecoder');
 export default class App extends React.Component {
   state = {
     loaded: false,
+    coords: [],
+    init_angle: 0.0,
+    got_angle: false,
   }
 
+  //before mounting view, do these things
   componentWillMount() {
     this.preloadAssetsAsync();
+    this._getLocationAsync();
   }
 
   async preloadAssetsAsync() {
@@ -25,8 +30,77 @@ export default class App extends React.Component {
     this.setState({ loaded: true });
   }
 
+  /*
+    Request coordinates information.
+    About: 
+    location.coords.heading
+    north is 0 degrees, east is 90 degrees, south is 180 degrees, and so on.
+
+    location.coords.accuracy
+    The radius of uncertainty for the location, measured in meters
+  */
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    //update every 500 milliseconds and every 0.2 meter movement
+    var first_point = true;
+    Location.watchPositionAsync({enableHighAccuracy : true, timeInterval: 500, distanceInterval: 0.2}, 
+      (location) => {
+        if(first_point){
+          first_point = false;
+          //Get the route to destination with Google Map Directions API
+          const mode = 'walking'; // other modes like 'driving';
+          const origin = location.coords.latitude + ',' + location.coords.longitude;
+          /*
+          TODO: the destination should be passed in as a prop, 
+          right now hard code to be somewhere in main quad
+          */
+          const destination = '37.4274821,-122.1702636';
+          const APIKEY = 'AIzaSyDigh9mURxqbuujMNcZMVGmzG5oFoYWNSA';
+          const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${APIKEY}&mode=${mode}`;
+
+          fetch(url)
+              .then(response => response.json())
+              .then(responseJson => {
+                  console.debug("Response Received!");
+                  if (responseJson.routes.length) {
+                      const c_arr = routeDecoder.decode(responseJson.routes[0].overview_polyline.points);
+                      //add origin to the head
+                      c_arr.unshift({latitude: location.coords.latitude, longitude: location.coords.longitude});
+                      //push destination to the tail
+                      c_arr.push({latitude: 37.4274821, longitude: -122.1702636}); // definition below
+                      this.setState({
+                          coords: c_arr,
+                      });
+                      //Utilize the coords somehow
+                      console.debug("CUR: " + location.coords.latitude + " " + location.coords.longitude);
+                      const nextStop = routeDecoder.inBetween(this.state.coords, {latitude: location.coords.latitude, longitude: location.coords.longitude});
+                      console.debug("nextStop: " + nextStop);
+                      if(nextStop){
+                        var angle2N = routeDecoder.findHeading({latitude: location.coords.latitude, longitude: location.coords.longitude}, nextStop);
+                        var angle_diff = angle2N - location.coords.heading;
+                        this.setState({init_angle: angle_diff, got_angle: true});
+                      }else{
+                        /*
+                        receiver deviated from route, set first point to true
+                        to re-request the route
+                        */
+                        first_point = true;
+                      }
+                  }
+              }).catch(e => {console.warn(e)});
+        }else{
+          //Do something about the AR graphics
+        }
+      });
+  };
+
   render() {
-    return this.state.loaded ? (
+    return this.state.loaded && this.state.got_angle ? (
       <Expo.GLView
         ref={(ref) => this._glView = ref}
         style={{ flex: 1 }}
@@ -69,16 +143,22 @@ export default class App extends React.Component {
     //TODO: take 1/10 of the screen roughly, tune later
     const geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.02, 100);
     const cube = new THREE.Mesh(geometry, materials);
-    cube.position.z = -0.4;
+    cube.position.z = -0.5;
+    //Put the first coin in the direction you are going to. Calculated based on GPS readings, probably not the best
+    var matched_position = routeDecoder.rotateVector([cube.position.z, 0], this.state.init_angle);
+    cube.position.x = matched_position[1];
+    cube.position.z = matched_position[0];
+    
+    console.debug("angle is: " + this.state.init_angle + "x is: " + cube.position.x + "z is: " + cube.position.z);
     scene.add(cube);
 
     const animate = () => {
       requestAnimationFrame(animate);
 
       //x direction is pointing right, y axis is pointing upword
-      cube.rotation.x += 0.01;
+      //cube.rotation.x += 0.01;
       //cube.rotation.x = Math.PI / 5.0 * 2.0;
-      cube.rotation.y += 0.02;
+      cube.rotation.y += 0.04;
 
       renderer.render(scene, camera);
       gl.endFrameEXP();
---
+++ screens/routeDecoder.js
@@ -0,0 +1,54 @@
+//Hao Wu: a simple module to decode google directions api response's polyline to array of coordinates
+module.exports = {
+	decode: function(t,e){for(var n,o,u=0,l=0,r=0,d= [],h=0,i=0,a=null,c=Math.pow(10,e||5);u<t.length;){a=null,h=0,i=0;do a=t.charCodeAt(u++)-63,i|=(31&a)<<h,h+=5;while(a>=32);n=1&i?~(i>>1):i>>1,h=i=0;do a=t.charCodeAt(u++)-63,i|=(31&a)<<h,h+=5;while(a>=32);o=1&i?~(i>>1):i>>1,l+=n,r+=o,d.push([l/c,r/c])}return d=d.map(function(t){return{latitude:t[0],longitude:t[1]}})},
+	// above function transforms something like this geocFltrhVvDsEtA}ApSsVrDaEvAcBSYOS_@... to an array of coordinates
+	inBetween: function(arr, point){
+		//TODO: Need some tuning, tolerate GPS error a little maybe
+		console.debug("cur loc: " + point.latitude + " " + point.longitude);
+		var i;
+		for(i = 0; i < arr.length - 1; i++){
+			console.debug("point1: " + arr[i].latitude + " " + arr[i].longitude + " - point2: " + arr[i+1].latitude + " " + arr[i].longitude);
+			if( point.latitude <= Math.max(arr[i].latitude, arr[i+1].latitude) &&
+			 	point.latitude >= Math.min(arr[i].latitude, arr[i+1].latitude) &&
+			 	point.longitude <= Math.max(arr[i].longitude, arr[i+1].longitude) &&
+			 	point.longitude >= Math.min(arr[i].longitude, arr[i+1].longitude) ) {
+				//if a point is between 2 points, return the later point, it's our next stop
+				return arr[i+1];
+			}
+		}
+		// Receiver currently not between any points
+		return null;
+	},
+
+	findHeading: function(from, to){
+		var p1 = {
+			x: 1,
+			y: 0
+		};
+		var p2 = {
+			x: to.latitude - from.latitude,
+			y: to.longitude - from.longitude
+		};
+		//Return the heading in degrees, wrt the true north
+		return ((Math.atan2(p2.y, p2.x) * 180 / Math.PI - Math.atan2(p1.y, p1.x) * 180 / Math.PI) + 360) % 360;
+	},
+
+	rotateVector: function(vec, ang)
+	{
+	    ang = -ang * (Math.PI/180);
+	    var cos = Math.cos(ang);
+	    var sin = Math.sin(ang);
+	    return new Array(Math.round(10000*(vec[0] * cos - vec[1] * sin))/10000, Math.round(10000*(vec[0] * sin + vec[1] * cos))/10000);
+	},
+};
+
+/*Could use the returned coords array like this:
+<MapView.Polyline
+    coordinates={[
+        {latitude: initial.latitude, longitude: initial.longitude}, // optional
+        ...this.state.coords,
+        {latitude: final.latitude, longitude: final.longitude}, // optional
+    ]}
+    strokeWidth={4}
+/>
+*/
