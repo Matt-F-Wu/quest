65da295941bb2bbc60a316af918eb40bca991499
Matt-F-Wu <haohao.wu@mail.utoronto.ca>
2018-02-20 01:22:06 -0800

    Add landing mask + treasure map: https://stackoverflow.com/questions/29586754/how-can-i-recreate-this-wavy-image-effect

--- constants/Colors.js
+++ constants/Colors.js
@@ -20,7 +20,7 @@ export default {
   blurText: 'rgba(255, 255, 255, 0.8)',
   blurOrange: 'rgba(255, 140, 0, 0.8)',
   textBGBlur: 'rgba(0, 0, 0, 0.6)',
-
+  blackMask: 'rgba(0, 0, 0, 0.7)',
   accentColor: 'grey',
 
   // backgroundColor: '#2C2C2C',    // Dark grey
--- sendQuestScreens/CameraLandingPage.js
+++ sendQuestScreens/CameraLandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, Image } from 'react-native';
 import Icon2 from 'react-native-vector-icons/SimpleLineIcons';
 import ProfileOverlay from '../components/SendQuest/ProfileOverlay';
 import CameraBase from '../components/SendQuest/CameraBase';
@@ -28,10 +28,12 @@ export default class CameraLandingPage extends CameraBase {
       const { navigate } = this.props.navigation;
       //Hao: the key is to pass functions bond to this screen as navigation parameters and call them from subsequent screens
       return [
-          <ProfileOverlay key={'p_overlay'}/>, 
+          <View key={'black_mask'} style={{position: 'absolute', height: '100%', width: '100%', backgroundColor: Colors.blackMask}}></View>,
+          <Image key={'treasure_map'} style={{position: 'absolute', width: '80%', left: '10%', alignSelf:'center'}} resizeMode='contain' source={require('../assets/images/treasureMap.gif')} />, 
+          <ProfileOverlay key={'p_overlay'}/>,
           <TouchableOpacity key={'main_send_button'} style={{alignSelf:'center', backgroundColor:'transparent'}} 
             onPress={() => {this.setState({mountCam: false}); navigate('Compose', {remount: this.remount, main_remount: this.remount})}}>
-            <Icon2 name="target" size={250} color={Colors.tintColor} />
+            <Icon2 name="target" size={250} color={Colors.tintColor}/>
           </TouchableOpacity>
       ];
     }
