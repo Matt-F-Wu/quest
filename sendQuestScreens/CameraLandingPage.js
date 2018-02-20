6ed183783bc2ff7889c2a76b179d20759878503d
Matt-F-Wu <haohao.wu@mail.utoronto.ca>
2018-02-20 13:49:34 -0800

    Use WebView to render wavy map

--- sendQuestScreens/CameraLandingPage.js
+++ sendQuestScreens/CameraLandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Image } from 'react-native';
+import { TouchableOpacity, View, Image, WebView } from 'react-native';
 import Icon2 from 'react-native-vector-icons/SimpleLineIcons';
 import ProfileOverlay from '../components/SendQuest/ProfileOverlay';
 import CameraBase from '../components/SendQuest/CameraBase';
@@ -29,7 +29,7 @@ export default class CameraLandingPage extends CameraBase {
       //Hao: the key is to pass functions bond to this screen as navigation parameters and call them from subsequent screens
       return [
           <View key={'black_mask'} style={{position: 'absolute', height: '100%', width: '100%', backgroundColor: Colors.blackMask}}></View>,
-          <Image key={'treasure_map'} style={{position: 'absolute', width: '80%', left: '10%', alignSelf:'center'}} resizeMode='contain' source={require('../assets/images/treasureMap.gif')} />, 
+          <View style={{position: 'absolute', left: '5%', width: '90%', height: '30%', alignSelf:'center'}}><WebView scrollEnabled={false} key={'treasure_map'} style={{width: '100%', height: '100%', backgroundColor: 'transparent'}} source={require('../assets/webviews/wavy_map.html')} /></View>, 
           <ProfileOverlay key={'p_overlay'}/>,
           <TouchableOpacity key={'main_send_button'} style={{alignSelf:'center', backgroundColor:'transparent'}} 
             onPress={() => {this.setState({mountCam: false}); navigate('Compose', {remount: this.remount, main_remount: this.remount})}}>
